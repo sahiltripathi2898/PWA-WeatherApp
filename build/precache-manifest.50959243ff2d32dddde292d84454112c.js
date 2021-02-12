7b0b986d5ce0cccd58af9bb540e6485ec0495b44
sahil <sahil.tripathi@gmail.com>
2021-02-12 13:20:10 +0530

    Okay

---
+++ build/precache-manifest.50959243ff2d32dddde292d84454112c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "152c68757300f9bcfde70ddf0b1e6b5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4586e30c8624ee1ea32f",
+    "url": "/static/css/main.7827da09.chunk.css"
+  },
+  {
+    "revision": "3696c363641e190a7541",
+    "url": "/static/js/2.d54fc02e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d54fc02e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4586e30c8624ee1ea32f",
+    "url": "/static/js/main.5e6dd5bb.chunk.js"
+  },
+  {
+    "revision": "ae594932184f88bfb438",
+    "url": "/static/js/runtime-main.9d9d98d4.js"
+  },
+  {
+    "revision": "a9f3ad49a2569ba3ea4daeeac93ec90a",
+    "url": "/static/media/bg.a9f3ad49.jpg"
+  }
+]);
